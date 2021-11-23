9486620ec078f0d3414d1b9f634d0a9c6350c42f
Juan Reyes <juanmanuelreyesarrambide@gmail.com>
2021-11-23 19:03:36 -0300

    Boton de Compra

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -3,24 +3,47 @@
 import { Link } from 'react-router-dom';
 import { useContext, useEffect, useState } from "react";
 //Diseño
-import { Button, Container, Divider, Grid, List, Typography } from "@mui/material";
+import { Alert, AlertTitle, Button, Container, Divider, Grid, List, Typography } from "@mui/material";
 //Archivos CartContext y CartItem
 import { CartContext } from "../../contexts/CartContext";
 import CartItem from "../cartItem/CartItem";
+//Firebase
+import { addDoc, collection } from "@firebase/firestore";
+import { dataBase } from "../../firebase/firebase";
 
 const Cart = () => {
 
     const { items, removeItem } = useContext(CartContext);
     const [total, setTotal] = useState(0);
+    const [compra, setCompra] = useState();
 
     useEffect(() => {
         let nuevoTotal = items.reduce((acc, item) => acc + (item.price * item.cantidad), 0);
         setTotal(nuevoTotal);
     }, [items])
 
+    const onComprar = async () => {
+
+        const cartItems = items.map((item) => ({ id: item.id, title: item.title, price: item.price, cantidad: item.cantidad }));
+
+        const docSnap = await addDoc(collection(dataBase, "cases"), {
+            items: cartItems,
+            total: total
+        });
+        setCompra(docSnap.id);
+    }
+
+
     return (
         <Container maxWidth="md" sx={{ marginTop: 2 }}>
             {
+                compra ?
+                <Alert severity="success">
+                    <AlertTitle>¡Felicidades!</AlertTitle>
+                    ¡Su compra fue realizada de forma exitosa!
+                </Alert>
+                
+                :
                 !items.length ?
                     <>
                         <Typography variant="h4">El carrito se encuentra Vacio.</Typography>
@@ -46,6 +69,7 @@ const Cart = () => {
                                 </Typography>
                             </Grid>
                         </Grid>
+                        <Button variant="contained" onClick={onComprar}>Comprar</Button>
                     </>
             }
         </Container >
